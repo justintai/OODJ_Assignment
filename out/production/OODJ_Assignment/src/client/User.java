99ee72615a1e3ad037d3155c035f6d0863d2131b
justintai <62212747+justintai@users.noreply.github.com>
2021-11-30 14:05:31 +0800

    update title

--- out/production/OODJ_Assignment/src/client/User.java
+++ out/production/OODJ_Assignment/src/client/User.java
@@ -6,8 +6,10 @@ import javax.swing.*;
 
 public class User {
 
+    private static String title = "COVID-19 Vaccination System";
+
     public static void main(String[] args) {
-        JFrame frame = new LoginGUI("COVID-19 Vaccination System");
+        JFrame frame = new LoginGUI(title);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
